e0c3db7102ae54bd80c3db8fab7ae4a30de83c8a
Toni Petrina <tonipetrina@hotmail.com>
2024-04-24 13:06:44 +0200

    SDD 2024 update

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-export default ({ children, style }) => {
+export default ({ children, style, color, layoutStyle }) => {
   const memoizedStyle = React.useMemo(() => {
     return {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 2,
+      height: "100%",
+      width: "100%",
+      backgroundColor: color,
       padding: "2em",
+      ...layoutStyle,
+    };
+  }, [layoutStyle]);
+
+  const memoizedSubStyle = React.useMemo(() => {
+    return {
       ...style,
     };
   }, [style]);
 
-  return <div style={memoizedStyle}>{children}</div>;
+  return (
+    <div style={memoizedStyle}>
+      <section style={memoizedSubStyle}>{children}</section>
+    </div>
+  );
 };
